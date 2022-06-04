fc0607323c316aa4dd830feb232e7840b42abbcf
Ayudhia Isnafiani Fanada <ayudhiaaif@gmail.com>
2022-06-04 19:20:59 +0700

    admin

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-const menu = document.getElementById('menu-label');
-const sidebar = document.getElementsByClassName('sidebar')[0];
+// const menu = document.getElementById('menu-label');
+// const sidebar = document.getElementsByClassName('sidebar')[0];
 
-menu.addEventListener('click', function() {
-    sidebar.classList.toggle('hide');
+// menu.addEventListener('click', function() {
+//     sidebar.classList.toggle('hide');
+// })
+
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function(){
+    document.querySelector("body").classList.toggle("active");
 })
